044ed3b15886e70c441ef0ddb43bea827e948502
Fatemeh <f.soroodi.s@gmail.com>
2023-06-20 12:35:45 +0330

    temperature and features were added on app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
+function formarDate(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    " Wednesday",
+    "Thursday",
+    " Friday",
+    "Saturday ",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hour}:${minute}`;
+  console.log(day);
+}
 
+function desplayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#cityName");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let localDateElement = document.querySelector("#local-date");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  localDateElement = formarDate(response.data.time * 1000);
+}
+
+let apiKey = "241b37tabc824f548d9of2bb0bbe3ed0";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={London}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(desplayTemperature);
